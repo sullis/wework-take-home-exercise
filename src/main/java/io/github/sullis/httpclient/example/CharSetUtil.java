cf82ec4bfb06e7093a886aa1fbcbaed6da59de38
Sean Sullivan <github@seansullivan.com>
2019-06-12 19:08:39 -0400

    add CharSetUtil

---
+++ src/main/java/io/github/sullis/httpclient/example/CharSetUtil.java
@@ -0,0 +1,10 @@
+package io.github.sullis.httpclient.example;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class CharSetUtil {
+    static public final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    private CharSetUtil() { /* private */ }
+}
