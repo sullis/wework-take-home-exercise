7e81db53e2dfe6922c2099ef83a500d24fbbff4a
Sean Sullivan <github@seansullivan.com>
2019-06-12 22:16:00 -0400

    use [commons-csv] to parse input file

--- src/main/java/io/github/sullis/httpclient/example/FileParser.java
+++ src/main/java/io/github/sullis/httpclient/example/FileParser.java
@@ -1,20 +1,45 @@
 package io.github.sullis.httpclient.example;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
-
 import com.google.common.collect.ImmutableList;
-import com.google.common.io.CharSource;
-import com.google.common.io.Files;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
 public class FileParser {
 
     public ImmutableList<URL> parse(java.io.File file) throws IOException {
-        CharSource source = Files.asCharSource(file, CharSetUtil.DEFAULT_CHARSET);
-        // the first line is header information. We don't need it.
-        source.readFirstLine();
-        // TODO : source.forEachLine();
-        return ImmutableList.of(); // FIXME
+        if (!file.exists()) {
+            throw new FileParseException("file does not exist");
+        }
+        FileReader reader = new FileReader(file);
+        CSVParser parser = CSVFormat.DEFAULT.parse(reader);
+        ImmutableList.Builder builder = ImmutableList.builder();
+        parser.iterator().forEachRemaining(record -> {
+            final long lineNum = parser.getCurrentLineNumber();
+            System.out.println(lineNum);
+            if (lineNum > 1) {
+                String website = record.get(1);
+                try {
+                    builder.add(new URL("https://" + website));
+                }
+                catch (MalformedURLException ex) {
+                    throw new FileParseException("line:" + lineNum, ex);
+                }
+            }
+        });
+        return builder.build();
     }
 
+    static class FileParseException extends RuntimeException {
+        public FileParseException(String msg) {
+            super(msg);
+        }
+
+        public FileParseException(String msg, Exception ex) {
+            super(msg, ex);
+        }
+    }
 }
---
+++ src/test/java/io/github/sullis/httpclient/example/FileParserTest.java
@@ -0,0 +1,20 @@
+package io.github.sullis.httpclient.example;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.net.URL;
+
+public class FileParserTest {
+    private File twelveUrls = new File("./src/test/resources/twelve-urls.txt");
+
+    @Test
+    public void happyPath() throws Exception {
+        ImmutableList<URL> urls = new FileParser().parse(twelveUrls);
+        assertEquals(12, urls.size());
+        assertEquals("[https://facebook.com/, https://twitter.com/, https://google.com/, https://youtube.com/, https://wordpress.org/, https://adobe.com/, https://blogspot.com/, https://wikipedia.org/, https://linkedin.com/, https://wordpress.com/, https://yahoo.com/, https://amazon.com/]",
+                urls.toString());
+    }
+}
