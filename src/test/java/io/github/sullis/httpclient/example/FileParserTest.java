aaf527721422fac2c21536a1ab180055fe2449c4
Sean Sullivan <github@seansullivan.com>
2019-06-16 14:35:25 -0400

    more tests [FileParserTest]

--- src/test/java/io/github/sullis/httpclient/example/FileParserTest.java
+++ src/test/java/io/github/sullis/httpclient/example/FileParserTest.java
@@ -1,5 +1,6 @@
 package io.github.sullis.httpclient.example;
 
+import com.google.common.collect.ImmutableList;
 import io.atlassian.fugue.Either;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +14,7 @@ public class FileParserTest {
     private File twelveUrls = new File("./src/test/resources/twelve-urls.txt");
 
     @Test
-    public void happyPath() throws Exception {
+    public void parseIteratorHappyPath() throws Exception {
         try (FileInputStream input = new FileInputStream(twelveUrls)) {
             Iterator<Either<LineStatus, URL>> iter = new FileParser().parse(input, CharSetUtil.DEFAULT_CHARSET);
             long urlCount = 0;
@@ -35,4 +36,20 @@ public class FileParserTest {
             assertFalse(iter.hasNext());
         }
     }
+
+    @Test
+    public void parseIteratorForEachRemaining() throws Exception {
+        ImmutableList.Builder urlListBuilder = ImmutableList.builder();
+        try (FileInputStream input = new FileInputStream(twelveUrls)) {
+            Iterator<Either<LineStatus, URL>> iter = new FileParser().parse(input, CharSetUtil.DEFAULT_CHARSET);
+            iter.forEachRemaining(line -> {
+                if (line.isRight()) {
+                    urlListBuilder.add(line.right().get());
+                }
+            });
+            assertFalse(iter.hasNext());
+        }
+        assertEquals(12, urlListBuilder.build().size());
+    }
+
 }
