545798ac02e78b746ff989ccdc001eedf9929beb
Sean Sullivan <github@seansullivan.com>
2019-06-16 18:32:18 -0400

    improve exit codes in Main.java

--- src/main/java/io/github/sullis/httpclient/example/Main.java
+++ src/main/java/io/github/sullis/httpclient/example/Main.java
@@ -21,15 +21,33 @@ public class Main {
       System.out.println("args: " + cmdLine.getArgList());
       final boolean verbose = cmdLine.hasOption("verbose");
       File f = new File("input.txt"); // FIXME
-      FileInputStream input = new FileInputStream(f);
-      FileParser parser = new FileParser();
-      Stream<Either<LineStatus, URL>> stream = parser.parse(input, CharSetUtil.DEFAULT_CHARSET);
-      Stream<URL> urlStream = stream.filter(item -> item.isRight()).map(item -> item.right().get());
-      TaskProcessor p = new TaskProcessor(HttpClientUtil.build(),
-              getListener(verbose),
-              urlStream,
-              20);
-      p.execute();
+      if (!f.exists()) {
+        System.err.println(f.toString() + " does not exist.");
+        System.exit(1);
+      }
+      if (f.isDirectory()) {
+        System.err.println(f.toString() + " is a directory.");
+        System.exit(1);
+      }
+      if (!f.canRead()) {
+        System.err.println("Unable to read file: " + f.toString());
+        System.exit(1);
+      }
+      if (f.length() == 0) {
+        System.err.println("Empty file: " + f.toString());
+        System.exit(1);
+      }
+      try (FileInputStream input = new FileInputStream(f)) {
+        FileParser parser = new FileParser();
+        Stream<Either<LineStatus, URL>> stream = parser.parse(input, CharSetUtil.DEFAULT_CHARSET);
+        Stream<URL> urlStream = stream.filter(item -> item.isRight()).map(item -> item.right().get());
+        TaskProcessor p = new TaskProcessor(HttpClientUtil.build(),
+                getListener(verbose),
+                urlStream,
+                20);
+        p.execute();
+        System.exit(0);
+      }
     } catch (ParseException ex) {
       System.err.println("Command line error:  " + ex.getMessage());
       System.exit(1);
