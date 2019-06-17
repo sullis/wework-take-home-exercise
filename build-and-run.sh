d53074dd2c7ad89f8c3819e939f29178d30581d5
Sean Sullivan <github@seansullivan.com>
2019-06-17 04:11:04 -0400

    tweak [build-and-run.sh]

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,3 +1,3 @@
 mvn clean package
 rm -f results.txt
-java -jar target/java-httpclient-example-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/java-httpclient-example-0.0.1-SNAPSHOT-jar-with-dependencies.jar --input urls.txt
