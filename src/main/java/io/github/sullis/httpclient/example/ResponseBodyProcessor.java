e4f80e60dfe2a960102364c6f199c57271d9f8ca
Sean Sullivan <github@seansullivan.com>
2019-06-16 20:49:05 -0400

    cleanup [ResponseBodyProcessor]

--- src/main/java/io/github/sullis/httpclient/example/ResponseBodyProcessor.java
+++ src/main/java/io/github/sullis/httpclient/example/ResponseBodyProcessor.java
@@ -3,5 +3,5 @@ package io.github.sullis.httpclient.example;
 import java.net.URL;
 
 public interface ResponseBodyProcessor {
-    public void processHttpResponse(URL url, int statusCode, String responseBody);
+    void processHttpResponse(URL url, int statusCode, String responseBody);
 }
