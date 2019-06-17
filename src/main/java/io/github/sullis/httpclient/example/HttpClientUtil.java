fe4faa77845d767a4cac19cc8dd63f8ce287a8d0
Sean Sullivan <github@seansullivan.com>
2019-06-17 04:55:59 -0400

    use [HttpClient.Redirect.NORMAL]

--- src/main/java/io/github/sullis/httpclient/example/HttpClientUtil.java
+++ src/main/java/io/github/sullis/httpclient/example/HttpClientUtil.java
@@ -11,7 +11,7 @@ public class HttpClientUtil {
     static public HttpClient build() {
         return HttpClient.newBuilder()
                 .connectTimeout(Duration.ofMillis(2000))
-                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .followRedirects(HttpClient.Redirect.NORMAL)
                 .build();
     }
 }
